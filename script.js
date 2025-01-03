3c403b645638f9b73d6b19a944eba8f669708880
Jagodish75 <joyprogramming75@gmail.com>
2025-01-03 11:21:04 +0600

    Adding script file

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.getElementById("btn").addEventListener("click", function () {
-    alert("Hello World");
-});
+const month = "march";
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
