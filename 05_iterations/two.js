a58fe980cd8a49c0fa25073ea6ca82b18d483dc3
Jagodish75 <joyprogramming75@gmail.com>
2025-01-03 21:28:43 +0600

    practise do while loop and while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,11 @@
+// let index = 0;
+// while (index < 10) {
+//     console.log(`Number is : ${index}`);
+//     index = index + 1;
+// }
+
+// let index = 11;
+// do {
+//     console.log(`My name is Jagodish`);
+//     index = index + 1;
+// } while (index < 5);
