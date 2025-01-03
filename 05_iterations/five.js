d6e85c78088b2c3d63a5d990541256ef2d561f8b
Jagodish75 <joyprogramming75@gmail.com>
2025-01-03 23:40:02 +0600

    hh

---
+++ 05_iterations/five.js
@@ -0,0 +1,13 @@
+// const myArr = ["jagodish", "kartick", "Gonesh", "lata"];
+// myArr.forEach((item, index, array) => {
+//     console.log(index, item, array);
+// });
+
+const myArray = [
+    { name: "Kartick", roll: 1 },
+    { name: "Gonesh", roll: 2 },
+    { name: "Jagodish", roll: 3 },
+];
+myArray.forEach((item) => {
+    console.log(item.name);
+});
