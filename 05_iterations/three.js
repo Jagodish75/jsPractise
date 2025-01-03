8405d4355a402f13dd45c0eeb492d27460dbd6a2
Jagodish75 <joyprogramming75@gmail.com>
2025-01-03 22:47:23 +0600

    for in loop practise

---
+++ 05_iterations/three.js
@@ -0,0 +1,26 @@
+// const arr = [1, 2, 3, 4, 5];
+// for (const element of arr) {
+//     console.log(element);
+// }
+
+// const greetting = "Happy new year";
+// for (const element of greetting) {
+//     console.log(element);
+// }
+
+// const map = new Map();
+// map.set("Bn", "Bangladesh");
+// map.set("pak", "Pakistan");
+
+// // console.log(map);
+// for (const [key, value] of map) {
+//     console.log(key, value);
+// }
+
+const myhObject = {
+    name: "Jagodish",
+    roll: 32,
+};
+for (const [key] of myhObject) {
+    console.log(key);
+}
