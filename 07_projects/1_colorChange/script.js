4c9d43ed42cd8fccdd0dd4701b446889857436d7
Jagodish75 <joyprogramming75@gmail.com>
2025-01-10 19:17:59 +0600

    Adding Finally color Changing Project

--- 07_projects/1_colorChange/script.js
+++ 07_projects/1_colorChange/script.js
@@ -5,18 +5,19 @@ buttons.forEach(function (button) {
     button.addEventListener("click", function (event) {
         // console.log(event);
         // console.log(event.target);
-        if (event.target.id === "grey") {
-            body.style.backgroundColor = "grey";
-        }
-        if (event.target.id === "pink") {
-            body.style.backgroundColor = event.target.id;
-        }
-        if (event.target.id === "green") {
-            body.style.backgroundColor = event.target.id;
-        }
-        if (event.target.id === "yellow") {
-            body.style.backgroundColor = event.target.id;
-        }
+        // if (event.target.id === "grey") {
+        //     body.style.backgroundColor = "grey";
+        // }
+        // if (event.target.id === "pink") {
+        //     body.style.backgroundColor = event.target.id;
+        // }
+        // if (event.target.id === "green") {
+        //     body.style.backgroundColor = event.target.id;
+        // }
+        // if (event.target.id === "yellow") {
+        //     body.style.backgroundColor = event.target.id;
+        // }
+        body.style.backgroundColor = event.target.id;
     });
     // console.log(button);
 });
